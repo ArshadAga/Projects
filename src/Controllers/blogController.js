5cb4e875ece927f863570be52fb6d88b99fb3ed0
rupamgupta-github <guptarupam.1020@gmail.com>
2022-11-14 15:53:01 +0530

    Put ,Delete and Middleware is Done

--- src/Controllers/blogController.js
+++ src/Controllers/blogController.js
@@ -2,7 +2,6 @@ const blogModel = require("../Models/blogModel");
 
 const createBlog = async function (req, res) {
   try {
-
   } catch (err) {
     return res.status(500).send({ status: false, err: err.message });
   }
@@ -10,7 +9,6 @@ const createBlog = async function (req, res) {
 
 const getBlogs = async function (req, res) {
   try {
-
   } catch (error) {
     res.status(500).send({ status: false, err: error.message });
   }
@@ -18,7 +16,45 @@ const getBlogs = async function (req, res) {
 
 const putBlog = async function (req, res) {
   try {
+    let data = req.body;
+    let authorId = req.query.authorId;
+    let id = req.params.blogId;
 
+    if (!id) {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          msg: "blogId must be present in request param ",
+        });
+    }
+
+    if (!authorId.isValidObjectId(id)) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "Please provide a Valid blogId" });
+    }
+
+    let blogFound = await blogModel.findOne({ _id: id });
+
+    if (!blogFound) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "No Blog with this Id exist" });
+    }
+
+    let updatedBlog = await blogModel.findOneAndUpdate(
+      { _id: id, authorId: authorId },
+      {
+        $addToSet: { tags: data.tags, subcategory: data.subcategory },
+        $set: { title: data.title, body: data.body, category: data.category },
+      },
+      { new: true, upsert: true }
+    );
+
+    if (updatedBlog) {
+      return res.status(200).send({ status: true, data: updatedBlog });
+    }
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -26,11 +62,59 @@ const putBlog = async function (req, res) {
 
 const deleteBlog = async function (req, res) {
   try {
+    let blog = req.params.blogId;
+    let authorId = req.query.authorId;
 
+    if (!blog) {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          msg: "blogId must be present in order to delete it",
+        });
+    }
+
+    if (!authorId.isValidObjectId(blog)) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "Please provide a Valid blogId" });
+    }
+
+    let blogFound = await blogModel.findOne({ _id: blog });
+
+    if (!blogFound) {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          msg: "No blog exists bearing this Blog Id, please provide another one",
+        });
+    }
+
+    if (blogFound.isdeleted === true) {
+      return res
+        .status(404)
+        .send({ status: false, msg: "this blog has been deleted by You" });
+    }
+
+    let deletedBlog = await blogModel.findOneAndUpdate(
+      { _id: blog },
+      { $set: { isdeleted: true }, deletedAt: Date.now() },
+      { new: true }
+    );
+
+    if (deletedBlog) {
+      return res
+        .status(200)
+        .send({
+          status: true,
+          msg: "Your Blog has been successfully deleted",
+          deletedData: deletedBlog,
+        });
+    }
   } catch (err) {
     res.status(500).send({ status: false, msg: err.message });
   }
 };
 
-
-module.exports = { createBlog, getBlogs, putBlog, deleteBlog};
+module.exports = { createBlog, getBlogs, putBlog, deleteBlog };
--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -2,6 +2,30 @@ const jwt = require("jsonwebtoken");
 
 const authorization = function (req, res, next) {
   try {
+    let token = req.headers["x-auth-token"];
+
+    if (!token) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "neccessary header token is missing" });
+    }
+
+    let decodeToken = jwt.verify(token, secretKey);
+    if (!decodeToken) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "this is an invalid token" });
+    }
+
+    let authorId = req.params.authorId;
+
+    if (!authorId) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "AuthorId is required to do this action" });
+    }
+    req.loggedIn = decodeToken;
+    next();
   } catch (err) {
     return res.status(500).send({ status: false, err: err.message });
   }
@@ -9,6 +33,12 @@ const authorization = function (req, res, next) {
 
 const authentication = function (req, res, next) {
   try {
+    if (req.loggedIn != authorId) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "You are not Valid User" });
+    }
+    next();
   } catch (err) {
     return res.status(500).send({ status: false, msg: err.message });
   }
